d1571a3dba89ad8b83fa409cae29e321d4fa33a4
Tuyishime Kyrie <tuyishimekyrie@gmail.com>
2024-07-10 15:30:31 +0200

    add localstorage to store the theme for the user

--- assets/js/main.js
+++ assets/js/main.js
@@ -169,6 +169,6 @@ themeButton.addEventListener('click', () => {
     document.body.classList.toggle(darkTheme)
     themeButton.classList.toggle(iconTheme)
     // We save the theme and the current icon that the user chose
-    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-theme', getCurrentTheme()) //localstorage for web browser
     localStorage.setItem('selected-icon', getCurrentIcon())
 })
